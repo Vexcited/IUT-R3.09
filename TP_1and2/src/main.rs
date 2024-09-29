4794913d627e873983bcbbf080951cc1f5de5d71
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-09-30 00:38:47 +0200

    refactor(main): use clap and clio to have proper support for file in args

--- TP_1and2/src/main.rs
+++ TP_1and2/src/main.rs
@@ -1,18 +1,75 @@
-use inquire::Text;
+use std::io::{self, Read, Write};
+use clap::{Parser, ValueEnum};
+use clio::*;
 
 mod vigenere;
-use vigenere::Vigenere;
+use vigenere::{vigenere_encrypt, vigenere_decrypt};
 
-fn main() {
-  let key = Text::new("Entrez la clé de chiffrement: ").prompt().unwrap();
-  let vigenere = Vigenere::new(&key);
+mod kasiski;
+use kasiski::kasiski_examination;
 
-  let plain_text = Text::new("Entrez le texte que vous voulez chiffrer: ").prompt().unwrap();
-  let cipher_text = vigenere.encipher(&plain_text);
-  println!("Texte chiffré: {}", cipher_text);
+/// CLI app that supports the kasiski and encrypt methods
+#[derive(Parser)]
+#[command(name = "TP_1and2")]
+struct Cli {
+  /// Method to use.
+  #[arg(value_enum)]
+  method: Method,
 
-  let cipher_text = Text::new("Entrez le texte que vous voulez déchiffrer: ").prompt().unwrap();
-  let plain_text = vigenere.decipher(&cipher_text);
-  println!("Texte déchiffré: {}", plain_text);
+  /// Input file, use '-' for stdin.
+  #[clap(value_parser, default_value="-")]
+  input: Input,
+
+  /// Output file '-' for stdout.
+  #[clap(long, short, value_parser, default_value="-")]
+  output: Output,
+
+  /// The encryption key (used only if method is encrypt or decrypt)
+  #[arg(long)]
+  key: Option<String>,
 }
 
+/// Enum representing available methods
+#[derive(Copy, Clone, ValueEnum)]
+enum Method {
+  Kasiski,
+  Encrypt,
+  Decrypt
+}
+
+fn main() -> io::Result<()> {
+  let mut cli = Cli::parse();
+
+  let mut input_data = String::new();
+  cli.input.read_to_string(&mut input_data)?;
+
+  let output_data = match cli.method {
+    Method::Kasiski => {
+      // let key_length = kasiski_examination(&input_data);
+      // println!("The most probable key length: {}", key_length);
+      std::process::exit(0);
+    },
+    Method::Encrypt => {
+      if let Some(key) = cli.key {
+        vigenere_encrypt(&input_data, &key)
+      }
+      else {
+        eprintln!("Error: Key is required for the encrypt method.");
+        std::process::exit(1);
+      }
+    }
+    Method::Decrypt => {
+      if let Some(key) = cli.key {
+        vigenere_decrypt(&input_data, &key)
+      }
+      else {
+        eprintln!("Error: Key is required for the decrypt method.");
+        std::process::exit(1);
+      }
+    }
+  };
+
+  cli.output.write_all(output_data.as_bytes())?;
+
+  Ok(())
+}
