ebfa824190a8ac7ab69b20334c31f7829a8f9090
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-09-30 09:00:02 +0200

    chore: typo

--- TP_1and2/src/input.rs
+++ TP_1and2/src/input.rs
@@ -3,6 +3,6 @@ pub fn normalize (message: &str) -> String {
   // On supprime les espaces, les caractères spéciaux et les chiffres du message.
   let message: String = message.chars().filter(|&c| c.is_ascii_alphabetic()).collect();
 
-  // On convertit la texte en minuscule.
+  // On convertit le texte en minuscule.
   message.to_ascii_lowercase()
 }
